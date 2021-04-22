ae6bc6126250b2791db9793bddab5898e0c66bec
Aaron Presley <a.presley@fetchrewards.com>
2021-04-22 09:47:21 -0700

    Moved the pseudo-translation command to its own set of files, in prep for more features

--- src/code.ts
+++ src/code.ts
@@ -1,44 +1,13 @@
-import { PseudoFormat } from 'i18n-pseudo';
-import { gatherTextNodes } from './utils';
+import pseudoTranslate from './pseudo-translate/code';
 
 (async () => {
-  const pseudo = new PseudoFormat();
-  const textNodes = gatherTextNodes(figma.currentPage.selection);
-  
-  if (!textNodes.length) {
-    figma.closePlugin('🚨 No text layers found in your selection')
+  switch(figma.command) {
+    case 'pseudo-translate':
+      pseudoTranslate()
+      break;
+    
+    default:
+      figma.closePlugin(`🚨 Unknown Command "${figma.command}"`);
+      break;
   }
-  
-  const loadedFonts:FontName[] = [];
-  const missingFontLayers:TextNode[] = [];
-  
-  for (let x = 0; x < textNodes.length; x += 1) {
-    const thisNode = textNodes[x];
-  
-    if (thisNode.hasMissingFont) {
-      console.warn(`Missing font for layer ${thisNode.name}`);
-      missingFontLayers.push(thisNode);
-      continue;
-    }
-  
-    if (!loadedFonts.includes(thisNode.fontName as FontName)) {
-      try {
-        await figma.loadFontAsync(thisNode.fontName as FontName);
-      } catch (err) {
-        console.error(`Problem loading font for layer ${thisNode.name}`);
-      }
-    }
-
-    const ogText = thisNode.characters;
-    const newText = pseudo.format(ogText);
-    thisNode.deleteCharacters(0, ogText.length);
-    thisNode.insertCharacters(0, newText, 'AFTER');
-  }
-
-  if (missingFontLayers.length) {
-    figma.closePlugin(`⚠️ Done, but couldn't translate ${missingFontLayers.length} layers with missing fonts.`);
-  } else {
-    figma.closePlugin(`✅ Done!`);
-  }
-  
 })();
---
+++ src/pseudo-translate/code.ts
@@ -0,0 +1,43 @@
+import { PseudoFormat } from 'i18n-pseudo';
+import { gatherTextNodes } from '../utils';
+
+export default async () => {
+  const pseudo = new PseudoFormat();
+  const textNodes = gatherTextNodes(figma.currentPage.selection);
+  
+  if (!textNodes.length) {
+    figma.closePlugin('🚨 No text layers found in your selection')
+  }
+  
+  const loadedFonts:FontName[] = [];
+  const missingFontLayers:TextNode[] = [];
+  
+  for (let x = 0; x < textNodes.length; x += 1) {
+    const thisNode = textNodes[x];
+  
+    if (thisNode.hasMissingFont) {
+      console.warn(`Missing font for layer ${thisNode.name}`);
+      missingFontLayers.push(thisNode);
+      continue;
+    }
+  
+    if (!loadedFonts.includes(thisNode.fontName as FontName)) {
+      try {
+        await figma.loadFontAsync(thisNode.fontName as FontName);
+      } catch (err) {
+        console.error(`Problem loading font for layer ${thisNode.name}`);
+      }
+    }
+
+    const ogText = thisNode.characters;
+    const newText = pseudo.format(ogText);
+    thisNode.deleteCharacters(0, ogText.length);
+    thisNode.insertCharacters(0, newText, 'AFTER');
+  }
+
+  if (missingFontLayers.length) {
+    figma.closePlugin(`⚠️ Done, but couldn't translate ${missingFontLayers.length} layers with missing fonts.`);
+  } else {
+    figma.closePlugin(`✅ Done!`);
+  }
+}
